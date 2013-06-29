cab175f3b4f44d0c1593ae3c0cc91916cfc80d91
Randy Secrist <randy.secrist@gmail.com>
2013-06-29 09:11:45 -0600

    remove support for original serialization

--- lib/ripple-encryption/json_serializer.rb
+++ lib/ripple-encryption/json_serializer.rb
@@ -56,63 +56,7 @@ module Ripple
       # @return [Object] the decrypted and deserialized object
       def load(object)
         # try the v2 (0.0.2) compatible way of deserialize first
-        begin
-          return EncryptedJsonDocument.new(@config, object).decrypt
-        # if that doesn't work, try the v1 (0.0.1) way
-        rescue OpenSSL::Cipher::CipherError, MultiJson::DecodeError
-          internal = decrypt(object)
-          return ::Riak::Serializers.deserialize('application/json', internal)
-        end
-      end
-
-      private
-
-      # generates a new iv each call unless a static (less secure)
-      # iv is used.
-      def encrypt(object)
-        old_version = '0.0.1'
-        result = ''
-        if cipher.respond_to?(:iv=) and @iv == nil
-          iv = OpenSSL::Random.random_bytes(cipher.iv_len)
-          cipher.iv = iv
-          result << old_version << iv
-        end
-
-        if cipher.respond_to?(:public_encrypt)
-          result << cipher.public_encrypt(object)
-        else
-          cipher_setup :encrypt
-          result << cipher.update(object) << cipher.final
-          cipher.reset
-        end
-        return result
-      end
-
-      def decrypt(cipher_text)
-        old_version = '0.0.1'
-
-        if cipher.respond_to?(:iv=) and @iv == nil
-          version = cipher_text.slice(0, old_version.length)
-          cipher.iv = cipher_text.slice(old_version.length, cipher.iv_len)
-          cipher_text = cipher_text.slice(old_version.length + cipher.iv_len, cipher_text.length)
-        end
-
-        if cipher.respond_to?(:private_decrypt)
-          cipher.private_decrypt(cipher_text)
-        else
-          cipher_setup :decrypt
-          result = cipher.update(cipher_text) << cipher.final
-          cipher.reset
-          result
-        end
-      end
-
-      def cipher_setup(mode)
-        cipher.send mode
-        cipher.key        = key        if key
-        cipher.iv         = iv         if iv
-        cipher.key_length = key_length if key_length
-        cipher.padding    = padding    if padding
+        return EncryptedJsonDocument.new(@config, object).decrypt
       end
     end
   end
