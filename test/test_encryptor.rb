f14ce07ff9654ee1bc7621d4dfd54c8676508201
Randy Secrist <randy.secrist@gmail.com>
2013-08-19 21:49:14 -0600

    binary string encoding change for ruby 2.0

--- test/test_encryptor.rb
+++ test/test_encryptor.rb
@@ -22,8 +22,8 @@ class TestEncryptor < MiniTest::Spec
       @encryptor = Ripple::Encryption::Encryptor.new config.to_h
       # example text
       @text      = "This is some nifty text."
-      # this is the example text encrypted
-      @blob      = "Vfn\xC3\xF1a\xB9\x89\x16\xCA\xD4w\xC4\xAF\x16\xA0c\xF7\xD0\x88\xA3;d\xC8Y\x91\xA8\x05W+)\xC8"
+      # this is the example text encrypted (binary string literals use UTF-8 in ruby 2.0
+      @blob      = "Vfn\xC3\xF1a\xB9\x89\x16\xCA\xD4w\xC4\xAF\x16\xA0c\xF7\xD0\x88\xA3;d\xC8Y\x91\xA8\x05W+)\xC8".force_encoding('ASCII-8BIT')
     end
 
     should "convert text to an encrypted blob" do
